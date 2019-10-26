f09ff6d3abf42d689657104516fc978970dfa22c
VISHNU KT <37811932+vishnukt@users.noreply.github.com>
2019-10-26 22:21:55 +0530

    Update exp23.py
    Proper indentation and spelling corrections.

--- Computer Science Workshop/exp23.py
+++ Computer Science Workshop/exp23.py
@@ -1,16 +1,16 @@
 23.Calculator
 
 def add(x,y):
- print "Sum oif two numbers is",(x+y)
+ print "Sum of two numbers is ",(x+y)
 def sub(x,y):
- print"Difference of two numbers is",(x-y)
+ print"Difference of two numbers is ",(x-y)
 def multi(x,y):
- print"Product of two number is",(x*y)
+ print"Product of two number is ",(x*y)
 def div(x,y):
  print"Quotient is",(x/y)
-x=input("Enter the value for x")
-y=input("Enter the value for y")
-z=raw_input("Enter your choice +,-,*,/: ")
+x=input("Enter the value for x : ")
+y=input("Enter the value for y : ")
+z=raw_input("Enter your choice +,-,*,/ : ")
 if z=='addition':
  add(x,y)
 elif z=='subtraction':
@@ -27,5 +27,5 @@ student@student-OptiPlex-3020:~/Desktop$ python calculator.py
 Enter the value for x4
 Enter the value for y5
 Enter your choice +,-,*,/: +
-Sum oif two numbers is 9
+Sum of two numbers is 9
 """
